c04e302efb5800acf0f36bd71294baa10dcbc7ed
AkAsH240904 <146891643+AkAsH240904@users.noreply.github.com>
2026-02-08 16:15:27 +0530

    port fix

--- src/app.js
+++ src/app.js
@@ -4,14 +4,14 @@ import connectDB from "./config/db.js";
 import bookingRoutes from "./routes/booking.routes.js";
 
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
 app.use("/", bookingRoutes);
 
 connectDB().then(() => {
-  app.listen(3000, () => {
+  app.listen(PORT, () => {
     console.log("Backend running on port 3000");
   });
 });
