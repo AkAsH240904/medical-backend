b033922f0ec0e16f53ce3510547a2ccadc824386
AkAsH240904 <146891643+AkAsH240904@users.noreply.github.com>
2026-02-08 16:41:40 +0530

    dotenv chanes

--- src/app.js
+++ src/app.js
@@ -1,33 +1,27 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
-dotenv.config();
+import bookingRoutes from "./routes/booking.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("Medical Booking API Running");
 });
+
+app.use("/", bookingRoutes);
 
 const PORT = process.env.PORT || 8080;
 
-const startServer = async () => {
-  try {
-    await connectDB();
-
+connectDB()
+  .then(() => {
     app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Backend running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-};
-
-startServer();
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err);
+  });
