13949fa2fa34dc708a8bc2c15dbcd88f81063f79
AkAsH240904 <146891643+AkAsH240904@users.noreply.github.com>
2026-02-08 16:32:33 +0530

    dotenv changes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import bookingRoutes from "./routes/booking.routes.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/", bookingRoutes);
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Backend running on port 3000");
-  });
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
+
+const PORT = process.env.PORT || 8080;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Backend running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
